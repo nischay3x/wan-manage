0a32ae84fbc5d3a1a9ebd2fa56f697bc2e90c606
Ihor Chumak <igorc@flexiwan.com>
2020-01-30 15:07:12 +0200

    Updated tokens with API implementation

--- backend/services/TokensService.js
+++ backend/services/TokensService.js
@@ -1,6 +1,10 @@
 /* eslint-disable no-unused-vars */
 const Service = require('./Service');
 
+const jwt = require('jsonwebtoken');
+const configs = require('../configs.js')();
+const Tokens = require('../models/tokens');
+
 class TokensService {
 
   /**
@@ -14,7 +18,17 @@ class TokensService {
     return new Promise(
       async (resolve) => {
         try {
-          resolve(Service.successResponse(''));
+        const result = await Tokens.find({ org: user.defaultOrg._id });
+
+          const tokens = result.map(item => {
+            return {
+              _id: item.id,
+              name: item.name,
+              token: item.token
+            };
+          });
+
+          resolve(Service.successResponse(tokens));
         } catch (e) {
           resolve(Service.rejectResponse(
             e.message || 'Invalid input',
@@ -31,11 +45,16 @@ class TokensService {
    * id String Numeric ID of the Token to delete
    * no response value expected for this operation
    **/
-  static tokensIdDELETE({ id }) {
+  static tokensIdDELETE({ id }, user) {
     return new Promise(
       async (resolve) => {
         try {
-          resolve(Service.successResponse(''));
+          const result = await Tokens.remove({
+            _id: id,
+            org: user.defaultOrg._id
+          });
+
+          resolve(Service.successResponse(null, 204));
         } catch (e) {
           resolve(Service.rejectResponse(
             e.message || 'Invalid input',
@@ -53,11 +72,27 @@ class TokensService {
    * tokenRequest TokenRequest  (optional)
    * returns Token
    **/
-  static tokensIdPUT({ id, tokenRequest }) {
+  static tokensIdPUT({ id, tokenRequest }, user) {
     return new Promise(
       async (resolve) => {
         try {
-          resolve(Service.successResponse(''));
+          await Tokens.update({
+            _id: id,
+            org: user.defaultOrg._id
+          }, { $set: tokenRequest }, { upsert: false, multi: false, runValidators: true, new: true });
+
+          const result = await Tokens.findOne({
+            _id: id,
+            org: user.defaultOrg._id
+          });
+
+          const token = {
+            _id: result.id,
+            name: result.name,
+            token: result.token
+          };
+
+          resolve(Service.successResponse(token));
         } catch (e) {
           resolve(Service.rejectResponse(
             e.message || 'Invalid input',
@@ -71,15 +106,29 @@ class TokensService {
   /**
    * Create new access token
    *
-   * id String Numeric ID of the Token to modify
    * tokenRequest TokenRequest  (optional)
    * returns Token
    **/
-  static tokensPOST({ id, tokenRequest }) {
+  static tokensPOST({ tokenRequest }, user) {
     return new Promise(
       async (resolve) => {
         try {
-          resolve(Service.successResponse(''));
+          const body = jwt.sign({
+            org: user.defaultOrg._id.toString(),
+            account: user.defaultAccount._id
+          }, configs.get('deviceTokenSecretKey'));
+
+          const token = await Tokens.create({
+            name: tokenRequest.name,
+            org: user.defaultOrg._id.toString(),
+            token: body
+          });
+
+          resolve(Service.successResponse({
+            _id: token.id,
+            name: token.name,
+            token: token.token
+          }));
         } catch (e) {
           resolve(Service.rejectResponse(
             e.message || 'Invalid input',
