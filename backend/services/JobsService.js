d59f5640a9bb78834c48b4a179eb038e487aacae
Ihor Chumak <igorc@flexiwan.com>
2020-02-08 23:59:04 +0200

    Added Jobs object

--- backend/services/JobsService.js
+++ backend/services/JobsService.js
@@ -1,5 +1,11 @@
 /* eslint-disable no-unused-vars */
 const Service = require('./Service');
+const configs = require('../configs')();
+const deviceQueues = require('../utils/deviceQueue')(
+  configs.get('kuePrefix'),
+  configs.get('redisUrl')
+);
+const logger = require('../logging/logging')({ module: module.filename, type: 'job' });
 
 class JobsService {
 
@@ -13,7 +19,17 @@ class JobsService {
    **/
   static async jobsGET({ offset, limit, status }, { user }) {
     try {
-      return Service.successResponse('');
+      // Generate and send the result
+      const result = [];
+      const stateOpts = ['complete', 'failed', 'inactive', 'delayed', 'active'];
+
+      await Promise.all(
+        stateOpts.map(async (s) => {
+          await deviceQueues.iterateJobsByOrg(user.defaultOrg._id.toString(), s,
+            (job) => result.push(job));
+        })
+      );
+      return Service.successResponse(200, result);
     } catch (e) {
       return Service.rejectResponse(
         e.message || 'Invalid input',
@@ -30,7 +46,16 @@ class JobsService {
    **/
   static async jobsIdDELETE({ id }, { user }) {
     try {
-      return Service.successResponse('');
+      logger.info('Deleting jobs', {
+        params: {
+          org: user.defaultOrg._id.toString(),
+          jobs: [ id ]
+        },
+        req: req
+      });
+
+      await deviceQueues.removeJobIdsByOrg(user.defaultOrg._id.toString(), [id]);
+      return Service.successResponse(204);
     } catch (e) {
       return Service.rejectResponse(
         e.message || 'Invalid input',
