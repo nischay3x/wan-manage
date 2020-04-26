c835e75cca4c8d9da605bfc0925c08560ff14a64
Omer Aharonian <omerah@flexiwan.com>
2020-04-26 10:26:46 +0300

    Multi link policy model unit tests

--- backend/models/validators.js
+++ backend/models/validators.js
@@ -138,7 +138,7 @@ const validateEmail = (mail) => { return !isEmpty(mail) && email.validate(mail);
 const validateLabelName = (name) => { return /^[a-z0-9-_ .]{3,30}$/i.test(name || ''); };
 const validateLabelColor = (color) => { return /^#[0-9A-F]{6}$/i.test(color); };
 
-const validatePolicyName = (name) => { return /^[a-z0-9-_ .]{3,15}$/i.test(name || ''); };
+const validatePolicyName = (name) => { return /^[a-z0-9-_ .]{3,50}$/i.test(name || ''); };
 const validateRuleName = (name) => { return /^[a-z0-9-_ .]{3,15}$/i.test(name || ''); };
 
 module.exports = {
