7219607b393fead30408121f0bf6abfce3b33289
Omer Aharonian <omerah@flexiwan.com>
2020-05-20 14:09:11 +0300

    Add migration code for multi link policies

--- backend/migrations/1589736098867-mlpolicies.js
+++ backend/migrations/1589736098867-mlpolicies.js
@@ -1,15 +1,70 @@
-/**
- * Make any changes you need to make to the database here
- */
+// flexiWAN SD-WAN software - flexiEdge, flexiManage.
+// For more information go to https://flexiwan.com
+// Copyright (C) 2019  flexiWAN Ltd.
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+const { membership } = require('../models/membership');
+const { devices } = require('../models/devices');
+const { addPerms, removePerms } = require('./utils/updatePerms');
+const logger = require('../logging/logging')({
+  module: module.filename,
+  type: 'migration'
+});
+
 async function up () {
-  // Write migration here
+  try {
+    // Add the "policies" document to all devices
+    await devices.updateMany(
+      {},
+      { $set: { 'policies.multilink': {} } },
+      { upsert: false }
+    );
+
+    // Add multilink to user permission
+    await addPerms(membership, 'mlpolicies');
+  } catch (err) {
+    logger.error('Database migration failed', {
+      params: {
+        collections: ['membership', 'devices'],
+        operation: 'up',
+        err: err.message
+      }
+    });
+  }
 }
 
-/**
- * Make any changes that UNDO the up function side effects here (if possible)
- */
 async function down () {
-  // Write migration here
+  try {
+    // Remove the "policies" document to all devices
+    await devices.updateMany(
+      {},
+      { $unset: { policies: '' } },
+      { upsert: false }
+    );
+
+    // Remove multi link policies permissions
+    await removePerms(membership, 'mlpolicies');
+  } catch (err) {
+    logger.error('Database migration failed', {
+      params: {
+        collections: ['membership', 'devices'],
+        operation: 'down',
+        err: err.message
+      }
+    });
+  }
 }
 
 module.exports = { up, down };
