d0055b605413c2055c3fdef0d2a194aaada58641
Shneor Pinsky <shneor521@gmail.com>
2020-12-10 15:02:52 +0200

    fix migration

--- backend/migrations/1607600926132-pci_to_dev_id.js
+++ backend/migrations/1607600926132-pci_to_dev_id.js
@@ -30,7 +30,7 @@ async function up () {
             delete i.pciaddr;
           }
 
-          return { ...i, devId };
+          return { ...i, devId: 'pci:' + devId };
         });
 
         await devices.updateOne(
