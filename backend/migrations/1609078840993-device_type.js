85d9080230ecc81552308615e9763753757a1e8f
Shneor Pinsky <shneor521@gmail.com>
2020-12-27 16:36:09 +0200

    fix lint

--- backend/migrations/1609078840993-device_type.js
+++ backend/migrations/1609078840993-device_type.js
@@ -15,7 +15,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 const { devices } = require('../models/devices');
-const { getOldInterfaceIdentification } = require('../deviceLogic/interfaces');
 const logger = require('../logging/logging')({ module: module.filename, type: 'migration' });
 
 async function up () {
