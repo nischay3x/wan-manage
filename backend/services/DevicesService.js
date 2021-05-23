4584ca33e21f7562810b0048ab49dfad787935ac
Shneor Pinsky <shneorp@flexiwan.com>
2021-05-23 13:01:44 +0300

    Fix lint issue

--- backend/services/DevicesService.js
+++ backend/services/DevicesService.js
@@ -466,7 +466,7 @@ class DevicesService {
                 deviceId: id, response: response.message
               }
             });
-            return Service.rejectResponse("Failed to get interface status", 500);
+            return Service.rejectResponse('Failed to get interface status', 500);
           } else {
             interfaceInfo = response.message;
           }
