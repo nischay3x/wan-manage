33a561b0ce0a10bf0686df4c91ddaba5c10ddc3c
Nir Ben-Dvora <nirbd@flexiwan.com>
2021-06-15 17:12:52 +0300

    fix job deletion timeout

--- backend/configs.js
+++ backend/configs.js
@@ -51,6 +51,8 @@ const configEnv = {
     userRefreshTokenExpiration: 604800,
     // The time to wait for job response before declaring the job as timeout
     jobTimeout: 180000,
+    // The time to retain jobs until deleted from the database, in msec
+    jobRetainTimeout: 604800000,
     // Key used for device tokens, override default with environment variable DEVICE_SECRET_KEY
     deviceTokenSecretKey: 'abcdefg1234567',
     // Key used to validate google captcha token, generated at https://www.google.com/u/1/recaptcha/admin/create
