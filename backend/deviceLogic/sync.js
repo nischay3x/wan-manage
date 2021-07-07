71ff6bfc55128cdc035a70fa8f3638d5a1712f19
Oleg Ferens <oleg.ferens@gmail.com>
2021-07-07 18:26:14 +0300

    Queue jobs priorities changed #364

--- backend/deviceLogic/sync.js
+++ backend/deviceLogic/sync.js
@@ -244,7 +244,7 @@ const queueFullSyncJob = async (device, hash, org) => {
       }
     },
     // Metadata
-    { priority: 'low', attempts: 1, removeOnComplete: false },
+    { priority: 'normal', attempts: 1, removeOnComplete: false },
     // Complete callback
     null
   );
