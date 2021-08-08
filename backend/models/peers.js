1ada41050675b64ba52790b54a0ed4435fa37f3d
Shneor Pinsky <shneorp@flexiwan.com>
2021-08-08 18:08:37 +0300

    URLs/IPs must be an Array

--- backend/models/peers.js
+++ backend/models/peers.js
@@ -66,22 +66,34 @@ const peerSchema = new Schema({
       message: 'Remote IP should be a valid ipv4 address'
     }
   },
-  urls: [{
-    type: String,
-    required: true,
+  urls: {
+    type: [{
+      type: String,
+      required: true,
+      validate: {
+        validator: validators.validateFQDN,
+        message: 'URL should be a valid FQDN'
+      }
+    }],
     validate: {
-      validator: validators.validateFQDN,
-      message: 'URL should be a valid FQDN'
+      validator: val => val.length > 0,
+      message: 'urls should be a list of URLs'
     }
-  }],
-  ips: [{
-    type: String,
-    required: true,
+  },
+  ips: {
+    type: [{
+      type: String,
+      required: true,
+      validate: {
+        validator: validators.validateIPv4,
+        message: 'IP should be a valid ip address'
+      }
+    }],
     validate: {
-      validator: validators.validateIPv4,
-      message: 'IP should be a valid ip address'
+      validator: val => val.length > 0,
+      message: 'ips should be a list of IP addresses'
     }
-  }]
+  }
 }, {
   timestamps: true
 });
