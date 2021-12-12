5bc1bc144ac646ccae4f4835a12f603cba14d2e2
Shneor Pinsky <shneorp@flexiwan.com>
2021-12-12 17:45:21 +0200

    Fix CR comments

--- backend/deviceLogic/modifyDevice.js
+++ backend/deviceLogic/modifyDevice.js
@@ -1054,6 +1054,14 @@ const apply = async (device, user, data) => {
   const org = data.org;
   const modifyParams = {};
 
+  device[0] = await device[0]
+    .populate('interfaces.pathlabels', '_id name type')
+    .execPopulate();
+
+  data.newDevice = await data.newDevice
+    .populate('interfaces.pathlabels', '_id name type')
+    .execPopulate();
+
   // Create the default/static routes modification parameters
   const modifyRoutes = prepareModifyRoutes(device[0], data.newDevice);
   if (modifyRoutes.routes.length > 0) modifyParams.modify_routes = modifyRoutes;
