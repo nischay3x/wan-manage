89537cc97dc6d9000847af67f9c8f4aff63c0772
Shneor Pinsky <shneorp@flexiwan.com>
2022-07-27 11:40:49 +0300

    Prevent BGP neighbors overlap tunnel range

---
+++ backend/migrations/1658903382566-remove_bgp_tunnel_neighbors.js
@@ -0,0 +1,67 @@
+
+// flexiWAN SD-WAN software - flexiEdge, flexiManage.
+// For more information go to https://flexiwan.com
+// Copyright (C) 2022  flexiWAN Ltd.
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+const { devices } = require('../models/devices');
+const logger = require('../logging/logging')({ module: module.filename, type: 'migration' });
+/**
+ * Make any changes you need to make to the database here
+ */
+async function up () {
+  try {
+    const res = await devices.collection.updateMany(
+      { 'bgp.neighbors': { $exists: true, $not: { $size: 0 } } },
+      [
+        {
+          $addFields: {
+            'bgp.neighbors': {
+              $filter: {
+                input: '$bgp.neighbors',
+                as: 'neighbor',
+                cond: {
+                  $not: {
+                    $regexMatch: {
+                      input: '$$neighbor.ip',
+                      regex: /^10\.100/
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      ],
+      { multi: true }
+    );
+    logger.info('Device bgp.neighbors database migration succeeded', {
+      params: { collections: ['devices'], operation: 'up', res }
+    });
+  } catch (err) {
+    logger.error('Device bgp.neighbors database migration failed', {
+      params: { collections: ['devices'], operation: 'up', err: err.message }
+    });
+    throw new Error(err.message);
+  }
+}
+
+/**
+ * Make any changes that UNDO the up function side effects here (if possible)
+ */
+async function down () {
+  return true;
+}
+
+module.exports = { up, down };
