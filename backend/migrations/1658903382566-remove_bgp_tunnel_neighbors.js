3e13d253cb292ce68345c38ce03ddfcfafd26eff
Shneor Pinsky <shneorp@flexiwan.com>
2022-09-08 15:12:22 +0300

    Add downgrade migration

--- backend/migrations/1658903382566-remove_bgp_tunnel_neighbors.js
+++ backend/migrations/1658903382566-remove_bgp_tunnel_neighbors.js
@@ -16,6 +16,8 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 const { devices } = require('../models/devices');
+const tunnelsModel = require('../models/tunnels');
+const { generateTunnelParams } = require('../utils/tunnelUtils');
 const logger = require('../logging/logging')({ module: module.filename, type: 'migration' });
 /**
  * Make any changes you need to make to the database here
@@ -61,7 +63,44 @@ async function up () {
  * Make any changes that UNDO the up function side effects here (if possible)
  */
 async function down () {
-  return true;
+  try {
+    const bgpTunnels = await tunnelsModel.find({
+      isActive: true,
+      peer: null,
+      'advancedOptions.routing': 'bgp'
+    })
+      .populate('deviceA', '_id bgp')
+      .populate('deviceB', '_id bgp')
+      .lean();
+
+    for (const bgpTunnel of bgpTunnels) {
+      const { num, deviceA, deviceB } = bgpTunnel;
+      const { ip1, ip2 } = generateTunnelParams(num);
+
+      const aAsn = deviceA.bgp.localASN;
+      const bAsn = deviceB.bgp.localASN;
+
+      // const remoteAsn = isDeviceA ? deviceB.bgp.localASN : deviceA.bgp.localASN;
+      await devices.updateOne(
+        { _id: deviceA._id },
+        { $push: { 'bgp.neighbors': { ip: ip2, remoteASN: bAsn } } }
+      );
+
+      await devices.updateOne(
+        { _id: deviceB._id },
+        { $push: { 'bgp.neighbors': { ip: ip1, remoteASN: aAsn } } }
+      );
+    }
+
+    logger.info('Device bgp.neighbors database migration succeeded', {
+      params: { collections: ['devices'], operation: 'up' }
+    });
+  } catch (err) {
+    logger.error('Device bgp.neighbors database migration failed', {
+      params: { collections: ['devices'], operation: 'down', err: err.message }
+    });
+    throw new Error(err.message);
+  }
 }
 
 module.exports = { up, down };
