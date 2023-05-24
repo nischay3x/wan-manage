b2254dab151e0d2d6223581cd683adc732d9c3f0
Shneor Pinsky <shneorp@flexiwan.com>
2023-05-24 15:13:50 +0300

    WIP - Adjust tests with new mongo error messages

--- backend/models/tests/tokenModel.test.js
+++ backend/models/tests/tokenModel.test.js
@@ -72,8 +72,8 @@ describe('Token schema', () => {
     tokenFullSchema.org = 'invalid-org';
 
     tokenFullSchema.validate((err) => {
-      expect(err.message).toBe('tokens validation failed: org: Cast to ObjectID failed ' +
-            'for value "invalid-org" at path "org"');
+      expect(err.message).toBe('tokens validation failed: org: Cast to ObjectId failed ' +
+            'for value "invalid-org" (type string) at path "org"');
     });
   });
 
