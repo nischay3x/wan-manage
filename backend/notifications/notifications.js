6840bec8ed1ae49f26536625b809a0716f329092
Shneor Pinsky <shneorp@flexiwan.com>
2023-10-18 11:04:25 +0300

    Add indexes to improve queries

--- backend/notifications/notifications.js
+++ backend/notifications/notifications.js
@@ -57,26 +57,26 @@ class Event {
     return [...parentNames];
   }
 
-  async getTarget (deviceId, interfaceId, tunnelId) {
+  getTarget (deviceId, interfaceId, tunnelId) {
     // MUST BE IMPLEMENTED IN CHILD CLASSES
   }
 
-  async getQuery (deviceId, interfaceId, tunnelId) {
+  getQuery (deviceId, interfaceId, tunnelId) {
     const query = [];
     const parentNames = this.getAllParents();
     if (parentNames.length === 0) {
-      query.push(await this.getTarget(deviceId, interfaceId, tunnelId));
+      query.push(this.getTarget(deviceId, interfaceId, tunnelId));
     }
     for (const parentName of parentNames) {
       const parent = hierarchyMap[parentName]; // Get the instance of the parent event
-      query.push(await parent.getTarget(deviceId, interfaceId, tunnelId));
+      query.push(parent.getTarget(deviceId, interfaceId, tunnelId));
     }
     return query;
   }
 }
 
 class DeviceConnectionEventClass extends Event {
-  async getTarget (deviceId, interfaceId, tunnelId) {
+  getTarget (deviceId, interfaceId, tunnelId) {
     return {
       eventType: this.eventName,
       'targets.deviceId': deviceId
@@ -85,7 +85,7 @@ class DeviceConnectionEventClass extends Event {
 }
 
 class RunningRouterEventClass extends Event {
-  async getTarget (deviceId, interfaceId, tunnelId) {
+  getTarget (deviceId, interfaceId, tunnelId) {
     return {
       eventType: this.eventName,
       'targets.deviceId': deviceId
@@ -94,7 +94,7 @@ class RunningRouterEventClass extends Event {
 }
 
 class InternetConnectionEventClass extends Event {
-  async getTarget (deviceId, interfaceId, tunnelId) {
+  getTarget (deviceId, interfaceId, tunnelId) {
     return {
       eventType: this.eventName,
       'targets.deviceId': deviceId,
@@ -104,7 +104,7 @@ class InternetConnectionEventClass extends Event {
 }
 
 class MissingInterfaceIPEventClass extends Event {
-  async getTarget (deviceId, interfaceId, tunnelId) {
+  getTarget (deviceId, interfaceId, tunnelId) {
     return {
       eventType: this.eventName,
       'targets.deviceId': deviceId,
@@ -114,7 +114,7 @@ class MissingInterfaceIPEventClass extends Event {
 }
 
 class TunnelStateChangeEventClass extends Event {
-  async getTarget (deviceId, interfaceId, tunnelId) {
+  getTarget (deviceId, interfaceId, tunnelId) {
     return {
       eventType: this.eventName,
       'targets.tunnelId': tunnelId
@@ -123,7 +123,7 @@ class TunnelStateChangeEventClass extends Event {
 }
 
 class LinkStatusEventClass extends Event {
-  async getTarget (deviceId, interfaceId, tunnelId) {
+  getTarget (deviceId, interfaceId, tunnelId) {
     return {
       eventType: this.eventName,
       'targets.deviceId': deviceId,
@@ -427,9 +427,9 @@ class NotificationsManager {
                 };
               }
             }
-            const eventParents = await event.getAllParents();
+            const eventParents = event.getAllParents();
             if (eventParents.length > 0) {
-              const parentsQuery = await event.getQuery(deviceId || targets.deviceId, interfaceId ||
+              const parentsQuery = event.getQuery(deviceId || targets.deviceId, interfaceId ||
                    targets.interfaceId, targets.tunnelId);
               const parentNotification = await notificationsDb.find(
                 { resolved, org, $or: parentsQuery });
