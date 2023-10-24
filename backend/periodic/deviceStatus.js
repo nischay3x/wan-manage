42d79f6d94f08498f45036c8cd837f1c074741a0
edenpe <edenp@flexiwan.com>
2023-10-24 21:25:33 +0300

    fix merge error (duplicated code)

--- backend/periodic/deviceStatus.js
+++ backend/periodic/deviceStatus.js
@@ -533,13 +533,7 @@ class DeviceStatus {
             logger.error(
               'Notification processing failed. Releasing lock & deleting key from Redis', {
                 params: { notificationKey, error: sendErr }
-            logger.error(
-              'Notification processing failed. Releasing lock & deleting key from Redis', {
-                params: { notificationKey, error: sendErr }
               });
-            this.removeNotificationKeyLock(notificationKey);
-          });
-      });
             this.removeNotificationKeyLock(notificationKey);
           });
       });
